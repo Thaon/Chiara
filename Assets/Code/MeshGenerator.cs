6cb5ab425010c29b61a5efeb79daeddcb17500ed
Thaon <gabriele.maddaloni@gmail.com>
2016-02-12 14:29:20 +0000

    Voxel geometry working, UVMap is skewed

--- Assets/Code/MeshGenerator.cs
+++ Assets/Code/MeshGenerator.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 
 [RequireComponent(typeof(MeshFilter))]
 [RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshCollider))]
 
 public class MeshGenerator : MonoBehaviour {
 
@@ -13,6 +14,10 @@ public class MeshGenerator : MonoBehaviour {
 	Mesh m_mesh;
 	List<Vector3> m_vertexList;
 	List<int> m_triIndexList;
+    int m_quadNumber = 0;
+
+    //mesh collision
+    MeshCollider m_collider;
 
 	//UV mapping
 	float m_tileSize; //used to specify how big each texture-slice is
@@ -33,6 +38,7 @@ public class MeshGenerator : MonoBehaviour {
 		m_UVList = new List<Vector2>();
 
 		m_mesh = GetComponent<MeshFilter>().mesh;
+        m_collider = GetComponent<MeshCollider>();
 
         m_tileSize = 0.5f;
 		
@@ -45,38 +51,97 @@ public class MeshGenerator : MonoBehaviour {
 		m_mesh.uv = m_UVList.ToArray();
 
 		m_mesh.RecalculateNormals();
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        m_collider.sharedMesh = null;
+        m_collider.sharedMesh = m_mesh;
+    }
+
+    // Update is called once per frame
+    void Update ()
     {
 	
 	}
 
 	void CreateVoxel(int x, int y, int z, float xOffset, float yOffset)
 	{
-		//add vertices to the vertexList
-		//starting with frontal face
-		m_vertexList.Add(new Vector3 (x, y+1, z));
-		m_vertexList.Add(new Vector3 (1, y+1, z));
-		m_vertexList.Add(new Vector3 (1, 0, z));
-		m_vertexList.Add(new Vector3 (0, 0, z));
 
+        CreateNegativeXFace(x, y, z, xOffset, yOffset);
+        CreatePositiveXFace(x, y, z, xOffset, yOffset);
+        CreateNegativeYFace(x, y, z, xOffset, yOffset);
+        CreatePositiveYFace(x, y, z, xOffset, yOffset);
+        CreateNegativeZFace(x, y, z, xOffset, yOffset);
+        CreatePositiveZFace(x, y, z, xOffset, yOffset);
+	}
 
-		//add indices to the triIndexList
-		//again, starting with front face
-		m_triIndexList.Add(0);
-		m_triIndexList.Add(1);
-		m_triIndexList.Add(3);
-		m_triIndexList.Add(1);
-		m_triIndexList.Add(2);
-		m_triIndexList.Add(3);
+    void CreateNegativeZFace(int x, int y, int z, float xOffset, float yOffset)
+    {
+        //add vertices to the vertexList
+        m_vertexList.Add(new Vector3(x, y + 1, z));
+        m_vertexList.Add(new Vector3(x + 1, y + 1, z));
+        m_vertexList.Add(new Vector3(x + 1, y, z));
+        m_vertexList.Add(new Vector3(x, y, z));
+
+        //we now add the triangles to the indices list
+        AddTriangleIndices();
 
         //we now take care of the UV
         SetUVCoords(xOffset, yOffset);
-	}
+    }
 
-	void SetUVCoords(float xOffest, float yOffset)
+    void CreatePositiveZFace(int x, int y, int z, float xOffset, float yOffset)
+    {
+        m_vertexList.Add(new Vector3(x + 1, y, z + 1));
+        m_vertexList.Add(new Vector3(x + 1, y + 1, z + 1));
+        m_vertexList.Add(new Vector3(x, y + 1, z + 1));
+        m_vertexList.Add(new Vector3(x, y, z + 1));
+        AddTriangleIndices();
+        SetUVCoords(xOffset, yOffset);
+    }    void CreateNegativeXFace(int x, int y, int z, float xOffset, float yOffset)
+    {
+        m_vertexList.Add(new Vector3(x, y, z + 1));
+        m_vertexList.Add(new Vector3(x, y + 1, z + 1));
+        m_vertexList.Add(new Vector3(x, y + 1, z));
+        m_vertexList.Add(new Vector3(x, y, z));
+        AddTriangleIndices();
+        SetUVCoords(xOffset, yOffset);
+    }    void CreatePositiveXFace(int x, int y, int z, float xOffset, float yOffset)
+    {
+        m_vertexList.Add(new Vector3(x + 1, y, z));
+        m_vertexList.Add(new Vector3(x + 1, y + 1, z));
+        m_vertexList.Add(new Vector3(x + 1, y + 1, z + 1));
+        m_vertexList.Add(new Vector3(x + 1, y, z + 1));
+        AddTriangleIndices();
+        SetUVCoords(xOffset, yOffset);
+    }    void CreateNegativeYFace(int x, int y, int z, float xOffset, float yOffset)
+    {
+        m_vertexList.Add(new Vector3(x, y, z));
+        m_vertexList.Add(new Vector3(x + 1, y, z));
+        m_vertexList.Add(new Vector3(x + 1, y, z + 1));
+        m_vertexList.Add(new Vector3(x, y, z + 1));
+        AddTriangleIndices();
+        SetUVCoords(xOffset, yOffset);
+    }    void CreatePositiveYFace(int x, int y, int z, float xOffset, float yOffset)
+    {
+        m_vertexList.Add(new Vector3(x, y + 1, z));
+        m_vertexList.Add(new Vector3(x, y + 1, z + 1));
+        m_vertexList.Add(new Vector3(x + 1, y + 1, z + 1));
+        m_vertexList.Add(new Vector3(x + 1, y + 1, z));
+        AddTriangleIndices();
+        SetUVCoords(xOffset, yOffset);
+    }
+
+    void AddTriangleIndices()
+    {
+        //add indices to the triIndexList
+        m_triIndexList.Add(m_quadNumber * 4);
+        m_triIndexList.Add((m_quadNumber * 4) + 1);
+        m_triIndexList.Add((m_quadNumber * 4) + 3);
+        m_triIndexList.Add((m_quadNumber * 4) + 1);
+        m_triIndexList.Add((m_quadNumber * 4) + 2);
+        m_triIndexList.Add((m_quadNumber * 4) + 3);
+        m_quadNumber++;
+    }
+
+    void SetUVCoords(float xOffest, float yOffset)
 	{
         //Debug.Log(m_tileSize + ": " + xOffest + ", " + yOffset);
         //Debug.Log((m_tileSize * m_tileXOffset) + m_tileSize);
